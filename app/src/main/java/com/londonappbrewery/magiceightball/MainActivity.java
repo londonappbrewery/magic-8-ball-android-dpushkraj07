92d9a048653749d329d92632496a71755ea8ec05
dpushkraj07 <dpushkraj@gmail.com>
2019-06-24 14:12:37 +0530

    Project Completed!!!!

--- app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
@@ -2,6 +2,12 @@ package com.londonappbrewery.magiceightball;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,50 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        Button ask = (Button) findViewById(R.id.askbutton);
+
+        final ImageView ball = (ImageView) findViewById(R.id.fortuneimage);
+
+        final int [] ballarr = {
+                R.drawable.ball1,
+                R.drawable.ball2,
+                R.drawable.ball3,
+                R.drawable.ball4,
+                R.drawable.ball5
+        };
+
+        ask.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d("Magic Eight Ball","The button has been pressed!");
+
+                Random randomnogen1 = new Random();
+
+                int no = randomnogen1.nextInt(5);
+
+                Log.d("Magic Eight Ball","The no generated on the ball is:"+no);
+
+                ball.setImageResource(ballarr[no]);
+
+
+
+            }
+        });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
 }
